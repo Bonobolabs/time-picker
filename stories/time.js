23aadeafbf74a212572a675f86bcb18031e0b970
Alex Louden <alex@louden.com>
2019-07-08 16:47:46 +0800

    Updated story

--- stories/time.js
+++ stories/time.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import moment from 'moment'
+
 // import { action } from '@storybook/addon-actions'
 // import {
 //   withKnobs,
@@ -11,4 +13,13 @@ import { storiesOf } from '@storybook/react'
 
 import TimePicker from '../src/'
 
-storiesOf('Timepicker', module).add('First', () => <TimePicker />)
+storiesOf('Timepicker', module).add('First', () => (
+  <TimePicker
+    minuteStep={15}
+    showSecond={false}
+    // show AM/PM
+    use12Hours={true}
+    // component manages state
+    defaultValue={moment()}
+  />
+))
