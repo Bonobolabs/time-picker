c4e3a4bbde5cac586d88a18dfca3a97ffbbc5806
Alex Louden <alex@louden.com>
2022-08-31 15:52:02 +0800

    update stories

--- stories/TimePicker.stories.jsx
+++ stories/TimePicker.stories.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { storiesOf } from '@storybook/react'
 import moment from 'moment'
 
-// import { action } from '@storybook/addon-actions'
-// import {
-//   withKnobs,
-//   text,
-//   boolean,
-//   number,
-//   selectV2
-// } from '@storybook/addon-knobs/react'
-
 import TimePicker from '../src'
 
-storiesOf('Timepicker', module).add('First', () => (
-  <TimePicker
-    minuteStep={15}
-    showSecond={false}
-    // show AM/PM
-    use12Hours={true}
-    // default to now
-    value={moment()}
-    onChange={v => console.log('onchange', v)}
-  />
-))
+const Picker = props => {
+  const [time, setTime] = useState(moment())
+
+  return (
+    <TimePicker
+      minuteStep={15}
+      showSecond={false}
+      // show AM/PM
+      use12Hours={true}
+      // default to now
+      value={time}
+      {...props}
+      onChange={value => {
+        console.log('onChange', value)
+        setTime(value)
+      }}
+    />
+  )
+}
+
+storiesOf('Timepicker', module)
+  .add('Step input only', () => <Picker />)
+  .add('Any input allowed', () => <Picker allowStepInputOnly={false} />)
+  .add('Null value', () => <Picker value={null} />)
